9a0e0b76ce79344a9e829e3c24f58964bec785f6
Akshay Kadam <akshaykadam0904@gmail.com>
2019-03-09 10:15:32 +0530

    Init 🚀

---
+++ background.js
@@ -0,0 +1,55 @@
+// NOTE: FOR CROSS PLATFORM (CHROME & FIREFOX) COMPATIBILITY 👇
+import browser from 'webextension-polyfill'
+
+// HACK: CHECK IF OBJECT IS EMPTY
+const isEmpty = obj =>
+  Object.keys(obj).length === 0 && obj.constructor === Object
+
+let data,
+  index,
+  extensions = []
+
+// NOTE: GETS CURRENT NEW TABS INDEX POSITION
+const getIndex = async () => {
+  data = await browser.storage.sync.get('index')
+  index = isEmpty(data) ? 0 : data.index
+}
+
+// DONE: GET ALL NEW TAB EXTENSIONS LIST
+const getAllExtensionsInfo = async () => {
+  const results = await browser.management.getAll()
+  extensions = results.filter(extension =>
+    extension.permissions.includes('newTabPageOverride'),
+  )
+}
+
+// NOTE: CALL IT ON EVERY NEW TAB CREATION
+const onNewTabCreation = () => {
+  enableOneNewTabExtension()
+  disableOtherNewTabExtensions()
+}
+
+// DONE: SET FIRST EXTENSION AS A NEW TAB WHEN NEW TAB IS CREATED & THEN KEEP ROTATING
+const enableOneNewTabExtension = async () => {
+  index = (index + 1) % extensions.length
+
+  await browser.management.setEnabled(extensions[index].id, true)
+  browser.storage.sync.set({
+    index: index + 1,
+  })
+}
+
+// DONE: DISABLE OTHER NEW TAB EXTENSIONS
+const disableOtherNewTabExtensions = async () => {
+  extensions.forEach((extension, i) => {
+    if (index !== i) browser.management.setEnabled(extension.id, false)
+  })
+}
+
+const main = () => {
+  getIndex()
+  getAllExtensionsInfo()
+  browser.tabs.onCreated.addListener(onNewTabCreation)
+}
+
+main()
